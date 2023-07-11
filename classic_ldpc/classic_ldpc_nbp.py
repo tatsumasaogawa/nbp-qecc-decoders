588922bb1b2e7cf4e19d804133217c8a02f9d3b5
tatsumasaogawa <tatsumasa.ogawa.p7@dc.tohoku.ac.jp>
2023-07-11 20:58:47 +0900

    modify classic_ldpc_nbp

--- classic_ldpc/classic_ldpc_nbp.py
+++ classic_ldpc/classic_ldpc_nbp.py
@@ -1,9 +1,6 @@
-# test
-# test2
-# test3
-
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 
 class BeliefPropagation(nn.Module):
@@ -11,17 +8,16 @@ class BeliefPropagation(nn.Module):
         super(BeliefPropagation, self).__init__()
         
         # 学習可能なパラメータの定義
-        self.b_v = nn.Parameter(torch.rand(num_nodes))
-        self.w = nn.Parameter(torch.rand(num_edges, num_edges))
+        self.b = nn.Parameter(torch.rand(num_nodes))
+        self.w = nn.Parameter(torch.rand(num_nodes, num_edges))
     
-    # とりあえず行列のサイズを合わせた (7/11)
-    def forward(self, l_v, s_c, iterations):
+    def forward(self, l_v, h, s_c, iterations):
         num_nodes = l_v.size(0)
-        num_edges = self.w.size(0)
+        num_edges = s_c.size(0)
         
         # メッセージ初期化
-        mu_c_to_v = torch.zeros(num_nodes, num_edges, dtype=torch.float32)
         mu_v_to_c = torch.zeros(num_nodes, num_edges, dtype=torch.float32)
+        mu_c_to_v = torch.zeros(num_edges, num_nodes, dtype=torch.float32)
         
         # メッセージ伝播ループ
         for t in range(iterations):
@@ -35,28 +31,84 @@ class BeliefPropagation(nn.Module):
             # print(self.b_v)
             # print(l_v.unsqueeze(1) * self.b_v)
             # print(torch.cat([self.b_v.unsqueeze(1), self.b_v.unsqueeze(1)], dim=1).size())
-            mu_v_to_c = l_v.unsqueeze(1) * torch.cat([self.b_v.unsqueeze(1), self.b_v.unsqueeze(1)], dim=1) + torch.matmul(mu_c_to_v, self.w)
+            mu_v_to_c_new  = torch.zeros(num_nodes, num_edges, dtype=torch.float32)
+            
+            for v in range(num_nodes):
+                for c in range(num_edges):
+                    mu_v_to_c_new[v, c] = self.update_message_v_to_c(v, c, l_v, h, mu_c_to_v, self.w, self.b)
             
             # メッセージ伝播 c -> v
             # print(s_c.unsqueeze(1))
             # print(torch.ones(num_nodes, num_edges, dtype=torch.float32))
-            a_mu_c_to_v = s_c.unsqueeze(0) * torch.ones(num_nodes, num_edges, dtype=torch.float32)
-            mu_c_to_v = a_mu_c_to_v
+            mu_c_to_v_new = torch.zeros(num_edges, num_nodes, dtype=torch.float32)
+
+            for c in range(num_edges):
+                # print(c)
+                for v in range(num_nodes):
+                    mu_c_to_v_new[c, v] = self.update_message_c_to_v(c, v, h, s_c, mu_v_to_c)
+            
+            mu_v_to_c = mu_v_to_c_new
+            mu_c_to_v = mu_c_to_v_new
         
         # 最終的なビリーフ計算
         # print(l_v)
         # print(self.b_v)
-        mu_v = l_v * self.b_v + torch.sum(torch.matmul(mu_c_to_v, self.w), dim=1)
+        mu_v = torch.zeros(num_nodes, dtype=torch.float32)
+
+        for v in range(num_nodes):
+            mu_v[v] = self.merginalization(v, l_v, h, mu_c_to_v, self.w, self.b)
         
-        return mu_v
+        sigma_mu_v = self.hf_sigmoid(mu_v)
+        
+        return sigma_mu_v
+    
+    def update_message_v_to_c(self, v, c, l_v, h, mu_c_to_v, w, b):
+        message = l_v[v] * b[v]
+
+        for c_prime in h[:, v]:
+            if c_prime != c:
+                # print(c_prime, v)
+                message += mu_c_to_v[c_prime, v] * w[v, c_prime]
+        
+        message = F.tanh(1 / 2 * message)
+
+        return message
+
+    def update_message_c_to_v(self, c, v, h, s, mu_v_to_c):
+        message = 1
+
+        # print(h[c, :])
+        for v_prime in h[c, :].tolist():
+            if v_prime != v:
+                message *= F.tanh(mu_v_to_c[v_prime, c] / 2)
+        
+        message = (-1) ** s[c] * 2 * torch.atanh(message)
+
+        return message
+    
+    def merginalization(self, v, l_v, h, mu_c_to_v, w, b):
+        message = l_v[v] * b[v]
+
+        for c in h[:, v]:
+            message += mu_c_to_v[c, v] * w[v, c]
+        
+        message = F.tanh(1 / 2 * message)
+
+        return message
+    
+    def hf_sigmoid(self, x):
+        return 1 / (torch.exp(x) + 1)
 
 # 使用例
 num_nodes = 3
 num_edges = 2
-l_v = torch.tensor([0.5, 0.3, 0.2], dtype=torch.float32)
-s_c = torch.tensor([0.7, 0.8], dtype=torch.float32)
+l_v = torch.tensor([4.6, 4.6, 4.6], dtype=torch.float32)
+s_c = torch.tensor([1, 0], dtype=torch.float32)
 iterations = 5
 e_v = torch.tensor([1, 0, 1], dtype=torch.float32)  # ターゲットの値
+
+h = torch.tensor([[1, 1, 0],  # パリティチェック行列
+                  [1, 0, 1]], dtype=torch.int32)
 
 model = BeliefPropagation(num_nodes, num_edges)
 criterion = nn.BCEWithLogitsLoss()  # 二値クロスエントロピー損失関数
@@ -64,9 +116,10 @@ optimizer = optim.Adam(model.parameters(), lr=0.01)  # 最適化手法
 
 for epoch in range(100):
     optimizer.zero_grad()
-    mu_v = model(l_v, s_c, iterations)
-    print(mu_v)
-    loss = criterion(mu_v, e_v)
+    # print(l_v[2])
+    sigma_mu_v = model(l_v, h, s_c, iterations)
+    # print(mu_v)
+    loss = criterion(sigma_mu_v, e_v)
     loss.backward()
     optimizer.step()
     
@@ -75,5 +128,8 @@ for epoch in range(100):
 
 # 学習後のパラメータの値
 print("Learned parameters:")
-print("b_v:", model.b_v.data)
+print("b_v:", model.b.data)
 print("w:", model.w.data)
+
+print('final solution:')
+print(sigma_mu_v.data)
