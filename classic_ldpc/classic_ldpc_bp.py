e660328a0dc8e0e672a65e4d8e78d6734d65c4eb
tatsumasaogawa <tatsumasa.ogawa.p7@dc.tohoku.ac.jp>
2023-07-13 10:25:58 +0900

    modify classic_ldpc_nbp & bp.py

--- classic_ldpc/classic_ldpc_bp.py
+++ classic_ldpc/classic_ldpc_bp.py
@@ -1,4 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import numpy as np
+from pyldpc.code import make_ldpc
+from pyldpc.encoder import encode_random_message
+from pyldpc.decoder import get_message
 
 
 class TannerGraph:
@@ -15,6 +22,10 @@ class TannerGraph:
             for variable_node in self.variable_nodes:
                 for check_node in variable_node.connected_check_nodes:
                     self.update_check_node(check_node, variable_node, syndrome)
+            
+            if self.check_convergence() == True:
+                print(i)
+                return
             
             # ma_check_node_message = 0 # 正規化
             # for check_node in self.check_nodes:
@@ -77,6 +88,23 @@ class TannerGraph:
 
     def hf_sigmoid(self, x):
         return 1 / (np.exp(x) + 1)
+    
+    def check_convergence(self):
+        for variable_node in self.variable_nodes:
+            messages_v_to_c_old_list = sorted(variable_node.messages_old.values())
+            messages_v_to_c_new_list = sorted(variable_node.messages_new.values())
+
+            if sum(np.isclose(messages_v_to_c_old_list, messages_v_to_c_new_list)) < len(messages_v_to_c_old_list):
+                return False
+
+        for check_node in self.check_nodes:
+                messages_c_to_v_old_list = sorted(check_node.messages_old.values())
+                messages_c_to_v_new_list = sorted(check_node.messages_new.values())
+
+                if sum(np.isclose(messages_c_to_v_old_list, messages_c_to_v_new_list)) < len(messages_c_to_v_old_list):
+                    return False
+        
+        return True
 
 class VariableNode:
     def __init__(self, index):
@@ -101,28 +129,77 @@ class CheckNode:
         self.connected_variable_nodes.append(variable_node)
         self.messages_old[str(self.index) + ',' + str(variable_node.index)] = 0
 
+# 使用例
+n = 30
+d_v = 2
+d_c = 3
+seed = np.random.RandomState(42)
+##################################################################
+# First we create an LDPC code i.e a pair of decoding and coding matrices
+# H and G. H is a regular parity-check matrix with d_v ones per row
+# and d_c ones per column
+
+H, G = make_ldpc(n, d_v, d_c, seed=seed, systematic=True, sparse=True)
+# print('H')
+# print(H)
+H_tensor = torch.from_numpy(H.astype(np.int32))
+G_tensor = torch.from_numpy(G.astype(np.int32))
+
+k, n = H.shape
+print("Number of coded bits:", k)
+
+v, y = encode_random_message(G, 6) # v: オリジナル, y: ノイズの乗った符号
+y = np.where(y > 0, 0, 1)
+v_tensor = torch.from_numpy(v.astype(np.int32))
+y_tensor = torch.from_numpy(abs(y).astype(np.int32))
+l_v = torch.tensor([0.023] * n)
+
+# print(H_tensor)
+# print(y_tensor)
+s_c_tensor = torch.matmul(H_tensor, y_tensor)
+s_c_tensor %= 2
+print('s_c_tensor')
+print(s_c_tensor)
+
+y_true_tensor = abs(torch.matmul(G_tensor, v_tensor))
+y_true_tensor %= 2
+print('y')
+print(y_tensor)
+print('y_true')
+print(y_true_tensor)
+# exit()
+
+e_v_tensor = abs(y_true_tensor - y_tensor).to(torch.float32)
+print('e_v_tensor')
+print(e_v_tensor)
+# exit()
 
 # タナーグラフの構築
-variable_nodes = [VariableNode(0), VariableNode(1), VariableNode(2)]
-check_nodes = [CheckNode(0), CheckNode(1)]
+variable_nodes = []
+for i in range(n):
+    variable_nodes.append(VariableNode(i))
 
-variable_nodes[0].connect_check_node(check_nodes[0])
-variable_nodes[0].connect_check_node(check_nodes[1])
-variable_nodes[1].connect_check_node(check_nodes[0])
-variable_nodes[2].connect_check_node(check_nodes[1])
+check_nodes = []
+for i in range(k):
+    check_nodes.append(CheckNode(i))
 
-check_nodes[0].connect_variable_node(variable_nodes[0])
-check_nodes[0].connect_variable_node(variable_nodes[1])
-check_nodes[1].connect_variable_node(variable_nodes[0])
-check_nodes[1].connect_variable_node(variable_nodes[2])
+for i in range(k):
+    for j in range(n):
+        if H[i, j] == 1:
+            variable_nodes[j].connect_check_node(check_nodes[i])
+            check_nodes[i].connect_variable_node(variable_nodes[j])
 
-syndrome = [1, 1]
-log_error_rate = 4.6 # log(0.99 / 0.01)
+syndrome = s_c_tensor.tolist()
+log_error_rate = 4.2
 
 # タナーグラフ上でのBPアルゴリズムの実行と復号
 tanner_graph = TannerGraph(variable_nodes, check_nodes)
 tanner_graph.belief_propagation(syndrome)
 decoded_message, error_rate = tanner_graph.decode()
+
+# print("y_tensor:", y_tensor)
+# print("y_true_tensor:", y_true_tensor)
+print("e_v_tensor", e_v_tensor.data)
 
 print("Decoded message:", decoded_message)
 print("Error rate:", error_rate)
--- classic_ldpc/classic_ldpc_nbp.py
+++ classic_ldpc/classic_ldpc_nbp.py
@@ -2,6 +2,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
+from pyldpc.code import make_ldpc
+from pyldpc.encoder import encode_random_message, encode
+from pyldpc.decoder import get_message
+
+np.set_printoptions(suppress=True)
+torch.set_printoptions(sci_mode=False)
+
 
 class BeliefPropagation(nn.Module):
     def __init__(self, num_nodes, num_edges):
@@ -9,7 +17,11 @@ class BeliefPropagation(nn.Module):
         
         # 学習可能なパラメータの定義
         self.b = nn.Parameter(torch.rand(num_nodes))
-        self.w = nn.Parameter(torch.rand(num_nodes, num_edges))
+        self.w = nn.Parameter(torch.rand(num_edges, num_nodes))
+
+        # b, w を 1 に固定した場合 (BP と等価)
+        # self.b = torch.ones(num_nodes)
+        # self.w = torch.ones(num_edges, num_nodes)
     
     def forward(self, l_v, h, s_c, iterations):
         num_nodes = l_v.size(0)
@@ -63,73 +75,144 @@ class BeliefPropagation(nn.Module):
         return sigma_mu_v
     
     def update_message_v_to_c(self, v, c, l_v, h, mu_c_to_v, w, b):
-        message = l_v[v] * b[v]
+        num_edges = h.size(0)
 
-        for c_prime in h[:, v]:
-            if c_prime != c:
-                # print(c_prime, v)
-                message += mu_c_to_v[c_prime, v] * w[v, c_prime]
-        
-        message = F.tanh(1 / 2 * message)
+        message = l_v[v] * b[v]
+        h_row = h[:, v]
+
+        for c_prime in range(num_edges):
+            if h_row[c_prime] == 1 and c_prime != c:
+                # print(c)
+                # print('c_prime')
+                # print(c_prime)
+                # print('v')
+                # print(v)
+                message += mu_c_to_v[c_prime, v] * w[c_prime, v]
 
         return message
 
-    def update_message_c_to_v(self, c, v, h, s, mu_v_to_c):
+    def update_message_c_to_v(self, c, v, h, s_c, mu_v_to_c):
+        num_nodes = h.size(1)
+
         message = 1
+        h_line = h[c, :]
 
         # print(h[c, :])
-        for v_prime in h[c, :].tolist():
-            if v_prime != v:
+        for v_prime in range(num_nodes):
+            if h_line[v_prime] == 1 and v_prime != v:
                 message *= F.tanh(mu_v_to_c[v_prime, c] / 2)
         
-        message = (-1) ** s[c] * 2 * torch.atanh(message)
+        message = (-1) ** s_c[c] * 2 * torch.atanh(message)
 
         return message
     
     def merginalization(self, v, l_v, h, mu_c_to_v, w, b):
-        message = l_v[v] * b[v]
+        num_edges = h.size(0)
 
-        for c in h[:, v]:
-            message += mu_c_to_v[c, v] * w[v, c]
-        
-        message = F.tanh(1 / 2 * message)
+        message = l_v[v] * b[v]
+        h_row = h[:, v]
+        # print('h_row')
+        # print(h_row)
+
+        for c in range(num_edges):
+            if h_row[c] == 1:
+                message += mu_c_to_v[c, v] * w[c, v]
 
         return message
     
     def hf_sigmoid(self, x):
         return 1 / (torch.exp(x) + 1)
 
+
 # 使用例
-num_nodes = 3
-num_edges = 2
-l_v = torch.tensor([4.6, 4.6, 4.6], dtype=torch.float32)
-s_c = torch.tensor([1, 0], dtype=torch.float32)
+n = 30
+d_v = 2
+d_c = 3
+seed = np.random.RandomState(0)
+##################################################################
+# First we create an LDPC code i.e a pair of decoding and coding matrices
+# H and G. H is a regular parity-check matrix with d_v ones per row
+# and d_c ones per column
+
+H, G = make_ldpc(n, d_v, d_c, seed=seed, systematic=True, sparse=True)
+H_tensor = torch.from_numpy(H.astype(np.int32))
+G_tensor = torch.from_numpy(G.astype(np.int32))
+
+k = G.shape[1]
+v = np.random.randint(2, size=k)
+print(v)
+
+y = encode(G, v, 10 ** 100)
+print(y)
+
+y = np.where(y > 0, 0, 1)
+print(y)
+
+
+k, n = H.shape
+print("Number of coded bits:", n)
+
+v, y = encode_random_message(G, 6) # v: 送信語, y: 受信語 (注意: y の 1 は 0, -1 は 1 に対応!!)
+print(y)
+y = np.where(y > 0, 0, 1)
+
+v_tensor = torch.from_numpy(v.astype(np.int32))
+y_tensor = torch.from_numpy(y.astype(np.int32))
+l_v = torch.tensor([3.74899243611] * n)
+print('ここを見る')
+print(torch.matmul(H_tensor, torch.matmul(G_tensor, v_tensor.unsqueeze(1))) % 2)
+
+print(H_tensor.shape)
+print(y_tensor.shape)
+s_c_tensor = torch.matmul(H_tensor, y_tensor.unsqueeze(1))
+s_c_tensor %= 2
+print('s_c_tensor')
+print(s_c_tensor)
+print((-1) ** s_c_tensor[0])
+
+y_true_tensor = torch.matmul(G_tensor, v_tensor)
+y_true_tensor %= 2
+
+print(y_true_tensor)
+print(y_tensor)
+e_v_tensor = abs(y_true_tensor - y_tensor).to(torch.float32)
+print(e_v_tensor)
+
+if sum(e_v_tensor) == 0: # y が符号語のときはつまらないので, exit()
+    exit()
+
 iterations = 5
-e_v = torch.tensor([1, 0, 1], dtype=torch.float32)  # ターゲットの値
 
-h = torch.tensor([[1, 1, 0],  # パリティチェック行列
-                  [1, 0, 1]], dtype=torch.int32)
-
-model = BeliefPropagation(num_nodes, num_edges)
+model = BeliefPropagation(num_nodes=n, num_edges=k)
 criterion = nn.BCEWithLogitsLoss()  # 二値クロスエントロピー損失関数
 optimizer = optim.Adam(model.parameters(), lr=0.01)  # 最適化手法
+
+# print('Error pattern:', e_v_tensor.to(torch.int32).numpy())
+# sigma_mu_v = model(l_v=l_v, h=H_tensor, s_c=s_c_tensor, iterations=iterations)
+# print('sigma_mu_v:')
+# print(sigma_mu_v.numpy())
+# exit()
 
 for epoch in range(100):
     optimizer.zero_grad()
     # print(l_v[2])
-    sigma_mu_v = model(l_v, h, s_c, iterations)
-    # print(mu_v)
-    loss = criterion(sigma_mu_v, e_v)
+    sigma_mu_v = model(l_v=l_v, h=H_tensor, s_c=s_c_tensor, iterations=iterations)
+
+    loss = criterion(sigma_mu_v, e_v_tensor)
     loss.backward()
     optimizer.step()
     
-    if (epoch + 1) % 10 == 0:
-        print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
+    if epoch % 10 == 0:
+        print(f"Epoch: {epoch}, Loss: {loss.item()}")
+
 
 # 学習後のパラメータの値
-print("Learned parameters:")
-print("b_v:", model.b.data)
-print("w:", model.w.data)
+# print("Learned parameters:")
+# print("b_v:", model.b.data)
+# print("w:", model.w.data)
 
-print('final solution:')
-print(sigma_mu_v.data)
+print('Final loss:', loss.item())
+print('Error pattern:', e_v_tensor.to(torch.int32).numpy())
+
+decoded_message = np.where(sigma_mu_v < 0.5, 0, 1)
+print("Decoded message:", decoded_message)
